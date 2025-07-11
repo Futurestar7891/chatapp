16509a041baa1c8e95055a915d6b170ec6396d3d
Future star <gajanandmali69@gmail.com>
2025-07-11 16:27:08 +0530

    info component done

--- frontend/src/Components/userMessage/Fetchmessages.jsx
+++ frontend/src/Components/userMessage/Fetchmessages.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useCallback, useMemo, useContext } from "react";
 import Message from "./Message";
 import { StateContext } from "../../main";
-import UserStatus from "./UserStatus";
+import UserStatus from "../../../src/Components/userMessage/UserStatus"
 import MessageInput from "./MessageInput";
 import "../../Css/Fetchmessages.css";
 
