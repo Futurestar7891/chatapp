173bdacefe51eacb0b5fc8a32efcdec3f12b50b7
Future star <gajanandmali69@gmail.com>
2025-07-12 11:39:58 +0530

    url update

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,10 @@ const server = http.createServer(app);
 // Middleware
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin:
+      process.env.FRONTEND_URL ||
+      process.env.OldFRONTEND_URL ||
+      "http://localhost:5173",
     credentials: true,
   })
 );
